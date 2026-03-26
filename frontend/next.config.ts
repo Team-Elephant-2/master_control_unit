22aa7a70ad3e3d15589d2990817804ec8eebfc38
Dias Altay <dias@altay.contact>
2026-03-26 15:44:38 +0100

    fix: allow ngrok links to work

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['unremarkable-edmund-clothlike.ngrok-free.dev'],
   async rewrites() {
     return [
       {
